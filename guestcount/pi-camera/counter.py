671946e8b85cb7a207c4eea9f72982fa2b58ed4c
byron-mejia-castillo <106005773+byron-mejia-castillo@users.noreply.github.com>
2025-05-03 18:37:18 -0400

    Update counter.py

--- guestcount/pi-camera/counter.py
+++ guestcount/pi-camera/counter.py
@@ -9,20 +9,21 @@ from airtable import send_to_airtable
 
 # Config
 CX1, CX2 = 550, 720  # Center lines for counting
-MIN_CONF = 0.5       # Confidence threshold
+MIN_CONF = 0.5       # Confidence threshold (.5 = 50%)
 FRAME_SIZE = (1280, 720)
 UPDATE_INTERVAL = 1200  # 20 minutes
 EST = timezone('US/Eastern')
 
+# keep track of temperature to make sure it can last the whole day
 def get_cpu_temp():
     try:
         return float(subprocess.getoutput("vcgencmd measure_temp | cut -f2 -d'=' | cut -f1 -d\"'\""))
     except:
         return 0.0
-
+# Operation hours are 10am-5pm est
 def should_run():
     now = datetime.now(EST)
-    return 10 <= now.hour < 22
+    return 10 <= now.hour < 17
 
 def main():
     if not should_run():
@@ -35,8 +36,8 @@ def main():
     picam2.configure(config)
     picam2.start()
 
-    model = YOLO('yolov8n.pt')
-    counts = {'in': 0, 'out': 0}
+    model = YOLO('yolov8n.pt') #yolov8n seems to have a nice balance of accuracy while not being super heavy on the pi 4
+    counts = {'in': 0, 'out': 0} # will increment when someone walks pasts the lines
     last_update = time.time()
     last_positions = {}
 
